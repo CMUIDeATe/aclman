dc72ddde7626e2dc3f8a63aaa62bfd7d21e09ab6
Tim Parenti <tparenti@andrew.cmu.edu>
2019-09-13 16:02:15 -0400

    Add new HL A31 resources to config.

--- aclman/config/development.py
+++ aclman/config/development.py
@@ -1,9 +1,10 @@
 # CSGold group mapping
 # Maps a `door_access` privilege value (room number) to a CSGold patron group number.
 csgold_group_mapping = {
-  'HL A10A': '789',
+  'HL A5': '791',
   'HL A10': '790',
-  'HL A5': '791'
+  'HL A10A': '789',
+  'HL A31': '1731'
 }
 
 # Grouper groups
@@ -17,6 +18,7 @@ mrbs_room_mapping = {
   'HL A5': 42,
   'HL A10': 60,
   'HL A10A': 56,
+  'HL A31': 69,
   'HL 106B': 58,
   'HL 106C': 59
 }
